37cc69f03981978c61da445fbd29e4db7b39e01f
Miguel <miramoss@unal.edu.co>
2025-09-04 01:45:07 -0500

    ejercicio_3

--- taller de practicar.py
+++ taller de practicar.py
@@ -1,5 +1,5 @@
 #miguel Ramos
-#Solución del taller de práctica del curso programacion orientada a objetos
+#Solución del taller de práctica del curso programacion orientada a objetos.
 
 #imprime un mensaje en la consola
 print("Hola, ya se imprimir mensajes en la consola")
@@ -10,3 +10,10 @@ b=532
 c=a+b
 print(f"La suma de {a} y {b} es {c}")
 
+#Escribir un programa que imprima por pantalla los números del 1 al 10 000.
+inicio=1
+fin=10000
+print("Los números del 1 al 10 000 son: ")
+for i in range(inicio, fin+1):
+    print(i)
+    
