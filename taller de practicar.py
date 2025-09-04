a273417dd18716d5f5b3ca79dbf3eb90ff1c2f8a
Miguel <miramoss@unal.edu.co>
2025-09-04 01:40:55 -0500

    ejercicio_2

--- taller de practicar.py
+++ taller de practicar.py
@@ -3,3 +3,10 @@
 
 #imprime un mensaje en la consola
 print("Hola, ya se imprimir mensajes en la consola")
+
+#Escribir un programa que imprima por pantalla la suma de 1234 y 532.
+a=1234 
+b=532
+c=a+b
+print(f"La suma de {a} y {b} es {c}")
+
